9d88c0cd8def48b4d055769aa77130e8619df6bf
Enkhjin <enkhjin.bayarkhuu@stud.fh-campuswien.ac.at>
2022-10-19 22:34:56 +0200

    Task 1 solved

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,16 +1,51 @@
 package at.ac.fhcampuswien;
 
-import java.util.Random;
+import java.text.DecimalFormat;
 import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.concurrent.atomic.DoubleAccumulator;
 
 public class App {
-
-    // Implement all methods as public static
-
+    //Exersice 1
     public static void main(String[] args) {
-        // test your method implementations here
-        // make method calls
-        // print their results
-        // etc.
+        oneMonthCalendar(31, 3);
+    }
+    public static void oneMonthCalendar(int numberDay, int startingDay) {
+        int line = 0;
+        int row = 0;
+        for (int j = 1; j <= 7; j++) {
+            if (j == startingDay) {
+                for (int i = 1; i <= numberDay; i++) {
+                    if (i <= 9) {
+                        System.out.print(" " + i + " ");
+                    } else {
+                        System.out.print(i + " ");
+                    }
+                    line++;
+                    if (line % 7 == 0) {
+                        if (row <= 4) {
+                            System.out.println("");}
+                        row++;
+
+                    }
+                }
+            } else if (line < startingDay) {
+                System.out.print("   ");
+                line++;
+            }
+            if (numberDay <= 2) {
+                System.out.print("");
+            }
+        }
+        if (line % 7 != 0) {
+            System.out.println();}
     }
 }
+
+
